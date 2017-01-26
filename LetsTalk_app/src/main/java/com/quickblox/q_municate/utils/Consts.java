b77759ed6dfc4073af53ecd32020dab0b882434e
Mohamed elmi <maxto024@gmail.com>
2017-01-26 22:24:14 +0600

    Final Commit

---
+++ LetsTalk_app/src/main/java/com/quickblox/q_municate/utils/Consts.java
@@ -0,0 +1,32 @@
+package com.quickblox.q_municate.utils;
+
+import android.graphics.Bitmap;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.quickblox.q_municate.R;
+
+public class Consts {
+
+    // Q-municate release
+    public static final String QB_APP_ID = "38298";
+    public static final String QB_AUTH_KEY = "yNmGXVm6jfkWLHX";
+    public static final String QB_AUTH_SECRET = "vcjcYOBtpBMhEeS";
+
+    public static final String QB_DOMAIN = "api.stage.quickblox.com";
+
+    // Universal Image Loader
+    public static final DisplayImageOptions UIL_DEFAULT_DISPLAY_OPTIONS = new DisplayImageOptions.Builder()
+            .cacheInMemory(true).imageScaleType(ImageScaleType.IN_SAMPLE_POWER_OF_2).cacheOnDisc(true).
+                    considerExifParams(true).bitmapConfig(Bitmap.Config.RGB_565).build();
+
+
+    public static final DisplayImageOptions UIL_USER_AVATAR_DISPLAY_OPTIONS = new DisplayImageOptions.Builder()
+            .showImageOnLoading(R.drawable.placeholder_user).showImageForEmptyUri(R.drawable.placeholder_user)
+            .showImageOnFail(R.drawable.placeholder_user).cacheOnDisc(true).cacheInMemory(true).considerExifParams(true).build();
+
+    public static final DisplayImageOptions UIL_GROUP_AVATAR_DISPLAY_OPTIONS = new DisplayImageOptions.Builder()
+            .showImageOnLoading(R.drawable.placeholder_group).showImageForEmptyUri(
+                    R.drawable.placeholder_group).showImageOnFail(R.drawable.placeholder_group).cacheOnDisc(
+                    true).cacheInMemory(true).considerExifParams(true).build();
+}
